bdb3e6b9771fe2662405a78ac05c5bee9efeae51
Michael D Hodge Jr <mikeh@lightsky.com>
2018-07-17 19:55:15 -0500

    fix to policy

--- src/api/policyApi.js
+++ src/api/policyApi.js
@@ -1,9 +1,5 @@
 class PolicyApi {
 
-  constructor() {
-    this.apiEndpoint = 'http://localhost:3001';
-  }
-
   static getAllPolicyCategories() {
     return fetch('http://localhost:3001/policy_categories').then(response => {
       return response.json();
